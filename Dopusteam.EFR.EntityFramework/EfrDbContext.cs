598c8c51c1d09919db2996c8d8391580e67e6f02
olegsafonov <safonov@visiology.com>
2017-09-14 22:34:34 +0300

    default schema name changed

--- Dopusteam.EFR.EntityFramework/EfrDbContext.cs
+++ Dopusteam.EFR.EntityFramework/EfrDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.Migrations.History;
 using Dopusteam.EFR.Core.Entities;
 
 namespace Dopusteam.EFR.EntityFramework
@@ -20,6 +21,16 @@ namespace Dopusteam.EFR.EntityFramework
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            var schemaName = "public";
+
+            // задаём название схемы БД
+            modelBuilder.HasDefaultSchema(schemaName);
+
+            // Настраиваем БД
+            // указываем название таблицы для хранение студентов
+            // это же можно сделать используя атрибуты в классе с сущностью
             modelBuilder.Entity<Student>()
                 .ToTable("Efr_Students");
 
@@ -27,11 +38,14 @@ namespace Dopusteam.EFR.EntityFramework
                 .ToTable("Efr_Groups");
 
             modelBuilder.Entity<Project>()
-                .ToTable("Efr_Projects")
-                .Property(p => p.Name);
+                .ToTable("Efr_Projects");
 
+            // Настриваем связи между сущностями (many-to-many)
+            // сущность студент...
             modelBuilder.Entity<Student>()
+                // имеет опциональную связь с таблицей групп...
                 .HasOptional(student => student.Group)
+                // каждя из которых сожерижт много студентов
                 .WithMany(group => group.Students);
 
             modelBuilder.Entity<Student>()
@@ -41,8 +55,6 @@ namespace Dopusteam.EFR.EntityFramework
                     .MapLeftKey("studentId")
                     .MapRightKey("projectId")
                     .ToTable("Efr_StudentsProjects"));
-
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
